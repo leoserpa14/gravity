0c3530cd876e6c12338e972f68f2540340df21ec
leoserpa14 <lscordeiro14@gmail.com>
2019-12-05 02:44:10 -0300

    Função força, move, update. Não funcionando corretamente

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ Config.read('config.ini')
 
 from kivy.app import App
 from kivy.uix.floatlayout import FloatLayout
-from kivy.uix.button import Button
+from kivy.clock import Clock
 from kivy.uix.widget import Widget
 from kivy.core.window import Window
 from kivy.lang.builder import Builder
@@ -18,11 +18,14 @@ class MainScreen(FloatLayout):
 	pass
 
 class Planet(Widget):
-	def __init__(self, radius, mass, x, y, **kwargs):
+	def __init__(self, radius, mass, x, y, vel, **kwargs):
 		super().__init__(**kwargs)
 		# Parametros que eu criei
 		self.radius = radius
 		self.mass = mass
+		self.vel = vel
+		self.ax = 0
+		self.ay = 0
 		# Parametros de Widget
 		self.size = self.radius, self.radius
 		self.center_x = x
@@ -30,31 +33,6 @@ class Planet(Widget):
 
 
 G = 6.71*(pow(10, -11)) # N * m² / kg²
-print(f'{G:e}')
-
-
-def force(planet1, planet2):
-
-	pos1 = [planet1.center_x, planet1.center_y]
-	pos2 = [planet2.center_x, planet2.center_y]
-	v = [pos2[0] - pos1[0], pos2[1] - pos1[1]]
-	print(v)
-
-	dist = sqrt( pow(v[0], 2) + pow(v[1], 2) )
-	print(dist)
-
-	# Força ocorrendo no planeta 1 pelos efeitos do planeta 2 tal que  F = G * M * m / d²
-	f12 = G * planet1.mass * planet2.mass / (pow(dist, 2))
-	print(f12)
-
-	#TODO: Estudar operação com vetores em numpy
-	return f12
-
-
-def move(planet1, planet2):
-	pass
-
-
 
 
 class GravityApp(App):
@@ -65,19 +43,56 @@ class GravityApp(App):
 		self.main = MainScreen()
 
 		# Objetos
-		self.sol = Planet(radius=50, mass=15, x=Window.width/2, y=Window.height/2)
-		self.terra = Planet(20, 5, Window.width*2/3, Window.height*3/4)
+		self.sol = Planet(radius=200.510, mass=1000000000000000, x=Window.width/2, y=Window.height/2, vel=[0, 0])
+		self.terra = Planet(radius=63.71, mass=5000000000, x=Window.width*2/3, y=Window.height*3/4, vel=[0, 0])
+
+		Clock.schedule_interval(self.update, 1.0 / 60.0)
 
 	def build(self):
 
 		self.main.add_widget(self.sol)
 		self.main.add_widget(self.terra)
-
-
-		force(self.terra, self.sol)
-
 		return self.main
 
+	def force(self, planet1, planet2):
+		pos1 = [planet1.center_x, planet1.center_y]
+		pos2 = [planet2.center_x, planet2.center_y]
+		v = [pos2[0] - pos1[0], pos2[1] - pos1[1]]
+		print(v)
+
+		dist = sqrt(pow(v[0], 2) + pow(v[1], 2))
+		print(dist)
+
+		# Força ocorrendo no planeta 1 pelos efeitos do planeta 2 tal que  F = G * M * m / d²
+		f12 = G * planet1.mass * planet2.mass / (pow(dist, 2))
+		print(f12)
+
+		# TODO: Estudar operação com vetores em numpy, talvez
+		return f12
+
+
+	def move(self, planet1, planet2):
+		# Mover o planeta 1 pela força realizada pelo planeta 2
+		f = self.force(planet1, planet2)
+		dist = sqrt(pow(planet1.center_x - planet2.center_x, 2) + pow(planet1.center_y - planet2.center_y, 2))
+
+		fx = f * (planet2.center_y - planet1.center_y) / dist
+		fy = f * (planet2.center_x - planet1.center_x) / dist
+
+		ax = fx / planet1.mass
+		ay = fy / planet1.mass
+
+		planet1.vel[0] = planet1.vel[0] + ax * 1
+		planet2.vel[1] = planet2.vel[1] + ay * 1
+
+		planet1.center_x = planet1.center_x + planet1.vel[0] * 1 + ax * (1) * (1) / 2
+		planet1.center_y = planet1.center_y + planet1.vel[1] * 1 + ay * (1) * (1) / 2
+
+
+		return
+
+	def update(self, dt):
+		self.move(self.terra, self.sol)
 
 
 if __name__ == '__main__':
